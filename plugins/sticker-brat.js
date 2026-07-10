6f33f2d7f37134e04f7e2631ab15810657337167
YALLICO OWNER <yallico2024@gmail.com>
2026-07-09 21:05:08 -0500

    Actualizar sticker-brat.js

--- plugins/sticker-brat.js
+++ plugins/sticker-brat.js
@@ -4,20 +4,20 @@ import { exec } from 'child_process'
 
 const handler = async (m, { conn, text }) => {
     let texto = text || m.quoted?.text
-    if (!texto) return m.reply('Escribe el texto.')
+    if (!texto) return m.reply('⚡ *RAYO PREM* ➔ Escribe el texto para el sticker.') // Cambiado
 
-    await m.react('🕒')
+    await m.react('⚡') // Cambiado
 
     let tmpImg = `./tmp-${Date.now()}.png`
     let tmpWebp = `./tmp-${Date.now()}.webp`
 
     try {
-        let endpoint = `https://sylphyy.xyz/tools/brat?text=${encodeURIComponent(texto)}&color=white&fondo=green&type=&api_key=sylphy-6f150d`
+        let endpoint = `https://sylphyy.xyz/tools/brat?text=${encodeURIComponent(texto)}&color=white&fondo=black&type=&api_key=sylphy-6f150d` // Cambiado fondo a black
         let response = await axios.get(endpoint, { responseType: 'arraybuffer' })
         fs.writeFileSync(tmpImg, response.data)
 
         await new Promise((resolve, reject) => {
-            exec(`ffmpeg -i ${tmpImg} -vcodec libwebp -vf "scale=512:512:force_original_aspect_ratio=decrease,format=rgba,pad=512:512:(ow-iw)/2:(oh-ih)/2:color=#00000000" ${tmpWebp}`, (err) => {
+            exec(`ffmpeg -i ${tmpImg} -vcodec libwebp -vf "scale=512:512:force_original_aspect_ratio=decrease,format=rgba,pad=512:512:(ow-iw)/2:(oh-ih)/2:color=#00000" ${tmpWebp}`, (err) => {
                 if (err) reject(err)
                 else resolve()
             })
@@ -25,13 +25,14 @@ const handler = async (m, { conn, text }) => {
 
         await conn.sendMessage(m.chat, { 
             sticker: fs.readFileSync(tmpWebp), 
-            packname: "Sasuke Bot", 
-            author: "Sebastián Barboza" 
+            packname: "Team Nightwish", // Cambiado
+            author: "Whois Yallico" // Cambiado
         }, { quoted: m })
 
-        await m.react('🔥')
+        await m.react('🌙') // Cambiado
     } catch (e) {
         await m.react('❌')
+        await m.reply('⚡ *RAYO PREM ERROR* ➔ Falló al generar el sticker brat.') // Cambiado
     } finally {
         if (fs.existsSync(tmpImg)) fs.unlinkSync(tmpImg)
         if (fs.existsSync(tmpWebp)) fs.unlinkSync(tmpWebp)
