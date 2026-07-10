4179f7f754fcd27186b7cba94b55dba4a8e80eb3
YALLICO OWNER <yallico2024@gmail.com>
2026-07-09 21:10:15 -0500

    Actualizar ia-gtp4.js

--- plugins/ia-gtp4.js
+++ plugins/ia-gtp4.js
@@ -1,15 +1,16 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, text }) => {
-  if (!text) return m.reply('Escribe algo para hablar con GPT-4.')
-  await m.react('⏳')
+  if (!text) return m.reply('⛈️ *RAYO PREM IA* ➔ Escribe tu consulta\n⚡ *Ejemplo:* .gpt4 que es un rayo') // Cambiado
+  await m.react('🌩️') // Cambiado
   let key = Buffer.from('c2FzdWtl', 'base64').toString('utf-8')
   let res = await fetch(`https://api.evogb.org/ai/gpt4-session?text=${encodeURIComponent(text)}&session=12&key=${key}`)
   let json = await res.json()
   if (json.status) {
-    await m.react('✅')
-    m.reply(json.result)
+    await m.react('⚡') // Cambiado
+    m.reply(`⛈️ *RAYO PREM IA* 🌙\n\n${json.result}\n\n⚡ *Team Nightwish*`) // Cambiado
   } else {
     await m.react('❌')
+    m.reply('⛈️ *RAYO PREM ERROR* ➔ No pude conectar con la IA') // Cambiado
   }
 }
 handler.help = ['gpt4 <texto>']
