d358ae3f52d33ef8830fdfc26b9b8e85edea74e4
YALLICO OWNER <yallico2024@gmail.com>
2026-07-19 21:18:32 -0500

    Eliminar main-ram.js

--- plugins/main-ram.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m) => {
-    const used = process.memoryUsage()
-    m.reply(`💻 *CYBER BOT* ➔ Consumo de Sistema\n⚡ *RAM Usada:* ${(used.heapUsed / 1024 / 1024).toFixed(2)} MB\n🤖 *Estado:* Sistema estable`) // Cambiado
-}
-handler.help = ['ram']
-handler.tags = ['main']
-handler.command = ['ram']
-export default handler
